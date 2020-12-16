6ace239b7cf7503fbd7577813ff307d374d64e7b
Alex Drozd <drozdster@gmail.com>
2020-12-16 22:10:12 +0100

    basic yml reading and writing

--- django_configuration_management/cli.py
+++ django_configuration_management/cli.py
@@ -1,9 +1,61 @@
 import click
 
-from django_configuration_management.utils import load_env
+from django_configuration_management.utils import (
+    decrypt_value,
+    dict_to_yml,
+    gather_user_input,
+    generate_fernet_key,
+    load_env,
+    yml_to_dict,
+)
 
 
-@click.command()
+@click.group()
+def manage():
+    pass
+
+
+@manage.command("upsert_secret")
 @click.option("--environment", default="development", help="Your environment")
-def main(environment):
+def upsert_secret(environment):
     load_env(environment)
+
+    data = yml_to_dict(environment)
+    key_name, key_value = gather_user_input()
+
+    data[key_name] = {"value": key_value, "secret": True}
+
+    dict_to_yml(data, environment)
+
+
+@manage.command("reveal_secrets")
+@click.option("--environment", default="development", help="Your environment")
+def reveal_secrets(environment):
+    load_env(environment)
+
+    data = yml_to_dict(environment)
+
+    for key, desc in data.items():
+        is_secret = desc.get("secret")
+
+        if not is_secret:
+            continue
+
+        value = desc["value"]
+        decrypted_value = decrypt_value(value)
+
+        print(f"{key}={decrypted_value}")
+
+
+@manage.command("generate_key")
+def generate_key():
+    key = generate_fernet_key()
+
+    print(f"Your key is: \n\n{key}\n")
+    print(
+        "Please store this in whichever .env-[environment] file you generated it for under the variable ENC_KEY"
+    )
+
+
+def main():
+    manage()
--- django_configuration_management/utils.py
+++ django_configuration_management/utils.py
@@ -1,6 +1,10 @@
 import os
+import re
+from getpass import getpass
 from pathlib import Path
 
+import yaml
+from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 
 
@@ -14,3 +18,54 @@ def load_env(environment):
         f"env not found: {env_path}. This file is required and must contain your ENC_KEY"
     )
     exit(1)
+
+
+def yml_to_dict(environment: str):
+    try:
+        with open(f"{environment}-config.yaml", "r") as yml:
+            loaded: dict = yaml.safe_load(yml)
+    except FileNotFoundError:
+        loaded = {}
+    return loaded
+
+
+def dict_to_yml(data: dict, environment: str) -> str:
+    with open(f"{environment}-config.yaml", "w+") as yml:
+        dumped = yaml.dump(data, yml)
+    return dumped
+
+
+def gather_user_input():
+    key_name = input("Enter key name: ")
+
+    is_valid = re.search("^[A-Z]+(?:_[A-Z]+)*$", key_name)
+
+    assert (
+        is_valid
+    ), f"Invalid key name. Keys must consist only of uppercase letters and underscore"
+
+    key_value = getpass("Enter key value: ")
+
+    return key_name, _encrypt_value(key_value)
+
+
+def _encrypt_value(value: str, encoding="utf-8"):
+    fernet = Fernet(os.getenv("ENC_KEY"))
+
+    encrypted = fernet.encrypt(bytes(value, encoding))
+
+    return encrypted.decode(encoding)
+
+
+def decrypt_value(value: str, encoding="utf-8"):
+    fernet = Fernet(os.getenv("ENC_KEY"))
+
+    decrypted = fernet.decrypt(bytes(value, encoding))
+
+    return decrypted.decode(encoding)
+
+
+def generate_fernet_key():
+    key = Fernet.generate_key()
+
+    return key.decode("utf-8")
