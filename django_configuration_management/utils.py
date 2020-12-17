17bd3c5638901d1e68c76262cd06818f790b3bc7
Alex Drozd <drozdster@gmail.com>
2020-12-18 00:40:09 +0100

    adding yml validator

--- django_configuration_management/utils.py
+++ django_configuration_management/utils.py
@@ -20,12 +20,25 @@ def load_env(environment):
     exit(1)
 
 
+def _validate_yml(data):
+    for key, meta in data.items():
+        _validate_key_name(key)
+        assert (
+            type(meta.get("secret")) == bool
+        ), f"{key} has an invalid row. Missing 'secret'"
+        assert (
+            type(meta.get("value")) == str
+        ), f"{key} has an invalid row. Missing 'value'"
+
+
 def yml_to_dict(environment: str):
     try:
         with open(f"{environment}-config.yaml", "r") as yml:
             loaded: dict = yaml.safe_load(yml)
     except FileNotFoundError:
         loaded = {}
+
+    _validate_yml(loaded)
     return loaded
 
 
@@ -35,14 +48,16 @@ def dict_to_yml(data: dict, environment: str) -> str:
     return dumped
 
 
+def _validate_key_name(key_name: str):
+    assert re.search(
+        "^[A-Z]+(?:_[A-Z]+)*$", key_name
+    ), f"Invalid key name {key_name}. Keys must consist only of uppercase letters and underscore"
+
+
 def gather_user_input():
     key_name = input("Enter key name: ")
 
-    is_valid = re.search("^[A-Z]+(?:_[A-Z]+)*$", key_name)
-
-    assert (
-        is_valid
-    ), f"Invalid key name. Keys must consist only of uppercase letters and underscore"
+    _validate_key_name(key_name)
 
     key_value = getpass("Enter key value: ")
 
