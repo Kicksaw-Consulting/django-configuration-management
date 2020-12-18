c35de19f6676bf4a8496fe508ac157dae5cdcc10
Alex Drozd <drozdster@gmail.com>
2020-12-18 18:24:37 +0100

    allowing numbers in key names

--- django_configuration_management/yml_utils.py
+++ django_configuration_management/yml_utils.py
@@ -60,5 +60,5 @@ def dict_to_yml(data: dict, environment: str) -> str:
 
 def validate_key_name(key_name: str):
     assert re.search(
-        "^[A-Z]+(?:_[A-Z]+)*$", key_name
+        "^[A-Z0-9]+(?:_[A-Z0-9]+)*$", key_name
     ), f"Invalid key name {key_name}. Keys must consist only of uppercase letters and underscore"
