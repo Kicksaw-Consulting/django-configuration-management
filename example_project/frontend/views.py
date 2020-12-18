7945017f84986c3ff4350446f84dface549aa796
Alex Drozd <drozdster@gmail.com>
2020-12-18 01:42:58 +0100

    fixing up the example project

--- example_project/frontend/views.py
+++ example_project/frontend/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {"username": settings.USERNAME, "password": settings.PASSWORD}
+    return render(request, "index.html", context)
