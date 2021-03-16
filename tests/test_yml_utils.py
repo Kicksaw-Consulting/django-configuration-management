e53bae0ab163e7314bc2683534adcd1202b3efe7
Alex Drozd <drozdster@gmail.com>
2021-03-16 01:08:28 +0100

    adding test for pull_aws_config_data

--- tests/test_yml_utils.py
+++ tests/test_yml_utils.py
@@ -2,12 +2,17 @@ from django_configuration_management.yml_utils import yml_to_dict
 
 
 def test_yml_to_dict():
-    data = yml_to_dict("test")
-    assert data == {
+    local, aws = yml_to_dict("test")
+    assert local == {
         "PASSWORD": {
             "secret": True,
             "value": "gAAAAABgPsL6dJtRSNdwf2lIV4xVZaBZl1ZTTA6TEriYouHt-IMh1fIxf18GPsqzxfaSyFNXiMMJ5xL2DwlQc8QdwPTOlhJGKQ==",
         },
         "USERNAME": "testusername",
-        "secrets/integrations/aws_secret": {"use_aws": True},
+    }
+    assert aws == {
+        "secrets/integrations/aws_secret": {
+            "use_aws": True,
+            "secret_keys": ["AWS_SECRET"],
+        },
     }
