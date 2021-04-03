6de107131f69a072659a036eee3bf425720c49d2
Alex Drozd <drozdster@gmail.com>
2021-04-03 15:14:11 +0200

    adding load_django to core

--- django_configuration_management/core.py
+++ django_configuration_management/core.py
@@ -1,3 +1,7 @@
+import os
+from typing import Callable
+import django
+
 from django.conf import settings
 
 from django_configuration_management.aws_utils import pull_aws_config_data
@@ -28,3 +32,21 @@ def inject_config(environment: str, settings_module: settings, dotenv_required=T
 
     for key, value in config.items():
         setattr(settings_module, key, value)
+
+
+def load_django(
+    path_to_settings_module: str,
+    pre_setup_hook: Callable = lambda *args, **kwargs: ...,
+    pre_setup_hook_args: list = [],
+    pre_setup_hook_kwargs: dict = {},
+    post_setup_hook: Callable = lambda *args, **kwargs: ...,
+    post_setup_hook_args: list = [],
+    post_setup_hook_kwargs: dict = {},
+):
+    """
+    For use in scripts that invokved via manage.py, e.g., lambda environments, non-management command CLI scripts, etc
+    """
+    pre_setup_hook(*pre_setup_hook_args, **pre_setup_hook_kwargs)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", path_to_settings_module)
+    django.setup()
+    post_setup_hook(*post_setup_hook_args, **post_setup_hook_kwargs)
